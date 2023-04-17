8400da404bfa5b34ffd06bbe4b3a5e0fa3abc6cf
heyolaniran <olaniran.abd@gmail.com>
2023-04-17 16:40:22 +0100

    lara-routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/blog')->name("blog.")
+->group(function () {
+    
+    Route::get("/" , function () { 
+        return  [
+            'link' => route("blog.show" , ['slug' => 'article-posted' , 'id' =>13])
+        ] ; 
+    })->name("index") ; 
+    
+    Route::get('/{slug}-{id}', function (string $slug , string $id) {
+        return [
+            'slug' => $slug , 
+            'id' => $id 
+        ] ; 
+    })->where([
+        'id' => '[0-9]+' , 
+        'slug' => '[a-z0-9\-]+'
+    ])->name("show") ; 
+
+}) ; 
+
+
