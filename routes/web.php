b324a4dcdce49ac3f4925ed55df093dadd800f64
heyolaniran <olaniran.abd@gmail.com>
2023-04-17 17:14:52 +0100

    eloquent ORM

--- routes/web.php
+++ routes/web.php
@@ -22,24 +22,33 @@ Route::prefix('/blog')->name("blog.")
 
     Route::get("/" , function () { 
 
-        $post = new Post() ; 
-        $post->title = "Mon premier article" ; 
-        $post->slug = "mon-premier-article" ; 
-        $post->content = " Le contenu de mon premier article" ; 
-        $post->save()  ; 
+        $posts = new Post() ; 
 
-        return $post  ; 
+        Post::create([
+            'title' => 'Nouvel article' , 
+            'slug' => 'nouvel-article' , 
+            'content' => "Contenu du nouvel article "
+        ]) ; 
         
+       
+
+        return $posts::where('id', '>', 0)->get() ; 
+
+        return $posts::all(['id' , 'title']) ; 
+
         return  [
             'link' => route("blog.show" , ['slug' => 'article-posted' , 'id' =>13])
         ] ; 
     })->name("index") ; 
     
     Route::get('/{slug}-{id}', function (string $slug , string $id) {
-        return [
-            'slug' => $slug , 
-            'id' => $id 
-        ] ; 
+       $post = Post::findOrFail($id) ; 
+
+       if($post->slug !== $slug) {
+           return to_route('blog.show', ['slug' => $post->slug , 'id' => $post->id]) ; 
+       }
+
+       return $post ; 
     })->where([
         'id' => '[0-9]+' , 
         'slug' => '[a-z0-9\-]+'
